71ba591879f56ee39c97eace7475b158d073f189
agco <agco7331@gmail.com>
2024-05-28 17:51:29 -0500

    Nuevo CÓdigo

---
+++ FormApp/src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+//Para mostrar elementos del menu hacemos lo siguiente
+interface MenuItem{
+  title: string;
+  route: string;
+}
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: [
+
+  ]
+})
+export class SideMenuComponent {
+  public reactiveMenu: MenuItem[] = [
+    {title: 'Basicos', route: './reactive/basic'},
+    {title: 'Dinamicos', route: './reactive/dynamic'},
+    {title: 'Switches', route: './reactive/switches'},
+  ]
+
+  public authMenu: MenuItem[] = [
+    {title: 'Registro', route: './auth'},
+
+  ]
+
+}
